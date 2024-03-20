757eecd41d621f3f164024c13572ea29657ebb55
Tonmoy <meheditonmoy2024@gmail.com>
2024-03-20 13:58:39 +0600

    start project

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+    return (
+        <div>
+            <span><small>Thanks for visiting myweb-site ..</small></span>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+            <h2>This is the Home component.</h2>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
